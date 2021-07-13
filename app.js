d460676aed4b60f655bd9846f98d1e812548bb94
mnew2m <mnew2m@gmail.com>
2021-07-13 13:57:48 +0900

    .

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    if(req.url == "/") {
+        res.write('<h1>Hello from nodejs</h1>');
+    } else {
+        res.write("<h1>You have entered invalid url</h1>");
+    }
+    res.end();
+});
+
+server.listen(8080,() => {
+    console.log("The server is listening on port 8080");
+});
